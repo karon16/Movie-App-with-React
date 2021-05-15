42b9a13dac4dba20f413741934714bccacb1e529
Christopher Buhendwa <christopherbuhendwa2000@gmail.com>
2021-05-15 11:56:53 +0100

    feat : add full responsiveness

--- src/Components/Shared/MovieHeroSection/MovieHeroSection.jsx
+++ src/Components/Shared/MovieHeroSection/MovieHeroSection.jsx
@@ -1,15 +1,4 @@
-import styled from "styled-components";
-
-const StyledMovieHeroSection = styled.section`
-  background: linear-gradient(0deg, rgba(14, 25, 48, 1) 11%, rgba(14, 25, 48, 0.7685324618128502) 45%, rgba(14, 25, 48, 0.2531262993478641) 95%),
-    url("${({ bg }) => bg}");
-  background-size: cover;
-  width: 100vw;
-  height: 70vh;
-  display: flex;
-  align-items: flex-end;
-  /* padding-top: 7% !important; */
-`;
+import { StyledMovieHeroSection } from "./MovieHeroSectionStyle";
 
 const MovieHeroSection = ({ children, bg }) => {
   return (
--- src/Components/Shared/MovieSectionTitle/MovieSectionTitle.jsx
+++ src/Components/Shared/MovieSectionTitle/MovieSectionTitle.jsx
@@ -1,15 +1,4 @@
-import styled from "styled-components";
-
-const StytledMovieSectionTitle = styled.h2`
-  color: white;
-  display: inline-block;
-  border-bottom: 10px solid ${({ theme }) => theme.colors.lightBlue};
-  font-family: ${({ theme }) => theme.fonts.roboto};
-  font-size: 5rem;
-  line-height: 7rem;
-  letter-spacing: 0.2rem;
-`;
-
+import { StytledMovieSectionTitle } from "./MovieSectionTitleStyle";
 const MovieSectionTitle = ({ children }) => {
   return <StytledMovieSectionTitle>{children}</StytledMovieSectionTitle>;
 };
